9984fcb81b8c815996b989caadc003f1ba9dbe3c
Gurkirat Singh <gurkiratsingh12a@gmail.com>
2024-04-17 14:41:43 +0530

    updating persistor

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Provider, persistor } from "react-redux";
-import { store } from "./app/store.js";
+import { Provider } from "react-redux";
+import { store, persistor } from "./app/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
