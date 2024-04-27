560916fa025c0ada541441b48e57fe3db6dcf657
Gurkirat Singh <gurkiratsingh12a@gmail.com>
2024-04-27 21:42:19 +0530

    Added delete and signout api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { updateUser } = require("../controller/user.controller.js");
+const {
+  updateUser,
+  deleteUser,
+  signout,
+} = require("../controller/user.controller.js");
 const { verifyToken } = require("../utils/verifyUser.js");
 
 router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser);
+router.post("/signout", signout);
 module.exports = router;
