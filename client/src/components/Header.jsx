31ef43faca40cc18f9cbe410fc38a5e064898d4e
Gurkirat Singh <gurkiratsingh12a@gmail.com>
2024-04-27 20:50:51 +0530

    completed responsive navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,43 +1,63 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import styles from "../styles/Header.module.css";
 import { IoIosSearch } from "react-icons/io";
 import Logo from "./Logo";
 import { useSelector } from "react-redux";
 import DropdownInfo from "./DropdownInfo";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { ImCross } from "react-icons/im";
 
 const Header = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const handleMenuClick = () => {
+    setShowMobileMenu((menu) => !menu);
+  };
+  const handleLinkClick = () => {
+    setShowMobileMenu(false);
+  };
   return (
-    <nav className={styles.navbar}>
-      <Logo />
-      <div className={styles.input}>
-        <input type="text" placeholder="Search" />
-        <span>
-          <IoIosSearch />
-        </span>
-      </div>
-      <div className={styles.navLinks}>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/projects">Projects</NavLink>
-          </li>
-          <li className={styles.btnGradient}>
-            {currentUser ? (
-              <DropdownInfo user={currentUser} />
-            ) : (
-              <NavLink to="/sign-in">Sign in</NavLink>
-            )}
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div style={{ marginBottom: "4.2rem" }}>
+      <nav className={styles.navbar}>
+        <Logo />
+        <div className={styles.input}>
+          <input type="text" placeholder="Search" />
+          <Link to="/search">
+            <IoIosSearch />
+          </Link>
+        </div>
+        <div className={styles.navLinks}>
+          <ul>
+            <div
+              className={`${styles.remove} ${
+                showMobileMenu ? styles.active : ""
+              }`}
+            >
+              <li onClick={handleLinkClick}>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li onClick={handleLinkClick}>
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li onClick={handleLinkClick}>
+                <NavLink to="/projects">Projects</NavLink>
+              </li>
+            </div>
+            <li className={styles.btnGradient}>
+              {currentUser ? (
+                <DropdownInfo user={currentUser} />
+              ) : (
+                <NavLink to="/sign-in">Sign in</NavLink>
+              )}
+            </li>
+            <span className={styles.hamburgerMenu} onClick={handleMenuClick}>
+              {showMobileMenu ? <ImCross /> : <GiHamburgerMenu />}
+            </span>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 };
 
