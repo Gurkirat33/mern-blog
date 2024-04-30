459acf5fe64b4a65862f3e4141c4da86322a61a0
Gurkirat Singh <gurkiratsingh12a@gmail.com>
2024-04-30 14:12:43 +0530

    Added show more posts functionality

--- client/src/components/PostsDash.jsx
+++ client/src/components/PostsDash.jsx
@@ -5,6 +5,7 @@ import styles from "../styles/PostDash.module.css";
 const PostsDash = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
   console.log(userPosts);
   useEffect(() => {
     const fetchPosts = async () => {
@@ -13,6 +14,9 @@ const PostsDash = () => {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error);
@@ -22,47 +26,76 @@ const PostsDash = () => {
       fetchPosts();
     }
   }, [currentUser._id]);
-
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className={styles.container}>
       {currentUser.isAdmin && userPosts.length > 0 ? (
-        <table className={styles.table}>
-          <thead>
-            <tr className={styles.tableHead}>
-              <th>Date updated</th>
-              <th>Post Image</th>
-              <th>Title</th>
-              <th>Catagory</th>
-              <th>Delete</th>
-              <th>Edit</th>
-            </tr>
-          </thead>
-          <tbody className={styles.tableBody}>
-            {userPosts.map((post) => (
-              <tr key={post._id} className={styles.row}>
-                <td>{new Date(post.createdAt).toLocaleDateString()}</td>
-                <td>
-                  <img
-                    src={post.image}
-                    style={{
-                      width: "4.8rem",
-                      aspectRatio: 1,
-                      objectFit: "cover",
-                    }}
-                  />
-                </td>
-                <td>{post.title}</td>
-                <td>{post.catagory}</td>
-                <td style={{ color: "var(--error-color)" }}>
-                  <p style={{ cursor: "pointer" }}>Delete</p>
-                </td>
-                <td style={{ color: "green" }}>
-                  <p style={{ cursor: "pointer" }}>Edit</p>
-                </td>
+        <>
+          <table className={styles.table}>
+            <thead>
+              <tr className={styles.tableHead}>
+                <th>Date updated</th>
+                <th>Post Image</th>
+                <th>Title</th>
+                <th>Catagory</th>
+                <th>Delete</th>
+                <th>Edit</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className={styles.tableBody}>
+              {userPosts.map((post) => (
+                <tr key={post._id} className={styles.row}>
+                  <td>{new Date(post.createdAt).toLocaleDateString()}</td>
+                  <td>
+                    <img
+                      src={post.image}
+                      style={{
+                        width: "4.8rem",
+                        aspectRatio: 1,
+                        objectFit: "cover",
+                      }}
+                    />
+                  </td>
+                  <td>{post.title}</td>
+                  <td>{post.catagory}</td>
+                  <td style={{ color: "var(--error-color)" }}>
+                    <p style={{ cursor: "pointer" }}>Delete</p>
+                  </td>
+                  <td style={{ color: "green" }}>
+                    <p style={{ cursor: "pointer" }}>Edit</p>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          {showMore && (
+            <div style={{ textAlign: "center", marginTop: "1rem" }}>
+              <button
+                className="btn"
+                style={{ width: "6rem" }}
+                onClick={handleShowMore}
+              >
+                Show more
+              </button>
+            </div>
+          )}
+        </>
       ) : (
         <div className={styles.noPost}>
           <p>No posts yet!</p>
