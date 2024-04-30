7e267124163130e303159c958e6d649b8e17f284
Gurkirat Singh <gurkiratsingh12a@gmail.com>
2024-04-30 13:55:39 +0530

    User posts in dashboard

--- client/src/components/PostsDash.jsx
+++ client/src/components/PostsDash.jsx
@@ -1,5 +1,78 @@
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "../styles/PostDash.module.css";
 const PostsDash = () => {
-  return <div>PostsDash</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+  console.log(userPosts);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
+
+  return (
+    <div className={styles.container}>
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <table className={styles.table}>
+          <thead>
+            <tr className={styles.tableHead}>
+              <th>Date updated</th>
+              <th>Post Image</th>
+              <th>Title</th>
+              <th>Catagory</th>
+              <th>Delete</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody className={styles.tableBody}>
+            {userPosts.map((post) => (
+              <tr key={post._id} className={styles.row}>
+                <td>{new Date(post.createdAt).toLocaleDateString()}</td>
+                <td>
+                  <img
+                    src={post.image}
+                    style={{
+                      width: "4.8rem",
+                      aspectRatio: 1,
+                      objectFit: "cover",
+                    }}
+                  />
+                </td>
+                <td>{post.title}</td>
+                <td>{post.catagory}</td>
+                <td style={{ color: "var(--error-color)" }}>
+                  <p style={{ cursor: "pointer" }}>Delete</p>
+                </td>
+                <td style={{ color: "green" }}>
+                  <p style={{ cursor: "pointer" }}>Edit</p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div className={styles.noPost}>
+          <p>No posts yet!</p>
+          <Link to="/create-post" className="btn btn-link">
+            Create post
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default PostsDash;
